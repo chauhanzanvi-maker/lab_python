86f81005c530be760c4d5412cca51599b6ed22d6
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-01-13 08:17:16 +0530

    Add files via upload

---
+++ practical_task_1.py
@@ -0,0 +1,32 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> p=float(input("Enter prinicipal amount:"))
+Enter prinicipal amount:1000
+>>> r=float(input("Enter rate:"))
+Enter rate:5
+>>> t=float(input("Enter time:"))
+Enter time:2
+>>> si=(p*r*t)/100
+>>> print("Simple Interest=",si)
+Simple Interest= 100.0
+>>> a=int(input("Enter first number:"))
+Enter first number:10
+>>> b=int(input("Enter second number:"))
+Enter second number:20
+>>> if a>b:
+...     print("maximum number is",a)
+... else:
+...     print("maximum number is",b)
+... 
+...     
+maximum number is 20
+>>> print("welcome to python programming...!!")
+welcome to python programming...!!
+>>> s=input("Enter a string:")
+Enter a string:python
+>>> print("first charchater is:",s[0])
+first charchater is: p
+>>> s=input("Enter a string:")
+Enter a string:python
+>>> print("last charchater is:",s[-1])
+last charchater is: n
