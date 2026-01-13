a6a1e3d136590731b652acd8d96d6ba730af8f79
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-01-13 08:32:47 +0530

    Add files via upload

---
+++ practical_task_2.py
@@ -0,0 +1,41 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+s=input("Enter a string:")
+Enter a string:python
+print("Length of string is:",len(s))
+Length of string is: 6
+num=int(input("Enter a number:"))
+Enter a number:3
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+... else:
+...     print("zero")
+... 
+...     
+positive number
+>>> a=int(input("Enter first number:"))
+Enter first number:5
+>>> b=int(input("Enter second number:"))
+Enter second number:10
+>>> c=int(input("Enter third number:"))
+Enter third number:15
+>>> 
+>>> total=a+b+c
+>>> print("sum=",total)
+sum= 30
+>>> name=("Enter your name:")
+>>> name=input("Enter your name:")
+Enter your name:zanvi
+>>> print("your name is",name)
+your name is zanvi
+>>> for i in range(1,6):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
