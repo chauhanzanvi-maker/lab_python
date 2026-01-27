2fcd4da10f1ff1f2d30facfe8c253c9dfff7961c
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-01-27 08:19:30 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,74 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#ARITHEMETIC OPERATOR
+a=5
+b=10
+print(a+b)
+15
+print(a-b)
+-5
+print(a/b)
+0.5
+print(a*b)
+50
+print(a%b)
+5
+print(a**b)
+9765625
+#ASSIGNMENT OPERATOR
+X=10
+print(X)
+10
+#UNARY MINUS
+A=5
+print(-A)
+-5
+#RELATIONAL OPERATOR
+p=10
+q=5
+print(p>q)
+True
+print(p==q)
+False
+print(p!=q)
+True
+print(p>=q)
+True
+print(p<=q)
+False
+#LOGICAL OPERATORS
+x=True
+y=False
+print(x and y)
+False
+print(x or y)
+True
+>>> print( not x)
+False
+>>> #BOOLEAN OPERATOR
+>>> IS_PASS=True
+>>> print(IS_PASS)
+True
+>>> #BITWISE OPERTORS
+>>> a=5
+
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a/b)
+1.6666666666666667
+>>> print(a^b)
+6
+>>> #MEBERSHIP OPERTORS
+>>> numbers=[10,20,30,40]
+>>> print(20 in numbers)
+True
+>>> print(50 not in numbers)
+True
+>>> #IDENTITY OPERATORS
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
