75ba8966841e4d40cd4b5e33cb415fba615a5918
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-01-30 20:17:52 +0530

    Add files via upload

---
+++ input_output_statements.py
@@ -0,0 +1,34 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("hello world")
+hello world
+name="zanvi"
+>>> age=18
+>>> print("NAME:",name)
+NAME: zanvi
+>>> print("AGE:",age)
+AGE: 18
+>>> name="zanvi"
+>>> age=18
+>>> name:zanvi
+>>> age:18
+>>> print("z","a","n","v","i")
+z a n v i
+>>> print("z","a","n","v","i",sep="-")
+z-a-n-v-i
+>>> name="zanvi"
+>>> marks=80
+>>> print(f"student{name}scored{marks}marks.")
+studentzanviscored80marks.
+>>> #input using input()
+>>> name=input("enter your name:")
+enter your name:zanvi
+>>> print("hello",name)
+hello zanvi
+>>> num=int(input("enter an integer:"))
+enter an integer:5
+>>> 
+>>> print("number",num)
+number 5
+>>> enter an integer:5
+SyntaxError: invalid syntax
