1c1d89b810d692844d02f4ac5358af39bb51a47b
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-01 22:05:20 +0530

    Add files via upload

---
+++ oi_statements.py
@@ -0,0 +1,40 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#OUTPUT USING PRINT()
+print("hello world...!!")
+hello world...!!
+name="zanvi"
+age=18
+print("NAME:",name)
+NAME: zanvi
+print("AGE:",age)
+AGE: 18
+>>> Name:zanvi
+>>> age:18
+>>> print("z","a","n","v","i")
+z a n v i
+>>> print("z","a","n","v","i",sep="-")
+z-a-n-v-i
+>>> name="zanvi"
+>>> marks=80
+>>> print(f"student{name}scored{marks}marks.")
+studentzanviscored80marks.
+>>> #INPUT USING PRINT()
+>>> name=input("enter your name:")
+enter your name:zanvi
+>>> print("hello",name)
+hello zanvi
+>>> num=int(input("enter an integer:"))
+enter an integer:5
+>>> print("Number=",num)
+Number= 5
+>>> price=float(input("enter price:"))
+enter price:15.50
+>>> print("price entered:",price)
+price entered: 15.5
+>>> first=input("enter first word:")
+enter first word:zanvi
+>>> second=input("enter second word:")
+enter second word:chauhan
+>>> print("you typed:",first," and ",second)
+you typed: zanvi  and  chauhan
