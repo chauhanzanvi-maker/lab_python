a16a94a11249a238cbdd1f1062724139c4a2b63c
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-14 08:22:45 +0530

    Add files via upload

---
+++ conditional_statement2.py
@@ -0,0 +1,9 @@
+marks=int(input("enter your marks:"))
+if marks>=90:
+    print("you will get a A grade.")
+elif marks>=70:
+    print("you will get  B grade. ")
+else:
+    print("you will get  C grade.")
+
+
