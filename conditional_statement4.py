62f9d3f15f0a0b5f16bf86a3e1f0616cd4a9eb55
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-16 21:10:08 +0530

    Add files via upload

---
+++ conditional_statement4.py
@@ -0,0 +1,13 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num=int(input("enter a number:"))
+enter a number:7
+>>> if num>0:
+...     print("positive number")
+... elif num<0:
+...     print("negative number")
+... else:
+...     print("zero")
+... 
+...     
+positive number
