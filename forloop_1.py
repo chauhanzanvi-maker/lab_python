1c055a6588edf7cb49bdbc8cecbb4ed32d002cd0
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-18 07:23:34 +0530

    Add files via upload

---
+++ forloop_1.py
@@ -0,0 +1,34 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+for i in range (1,6):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+>>> for i in range(3)
+SyntaxError: expected ':'
+>>> for i in range(3):
+...     print("hello")
+... 
+...     
+hello
+hello
+hello
+>>> for i in range (1,11):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
