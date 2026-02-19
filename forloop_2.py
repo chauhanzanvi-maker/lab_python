57baa57da42fd4f18ea84ecd6b0e67ab4fe76e16
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-19 11:29:44 +0530

    Add files via upload

---
+++ forloop_2.py
@@ -0,0 +1,44 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+for i in range(1,21):
+    if i % 2 == 0:
+        print(i)
+
+        
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+>>> for i in range (1,16):
+...     if i % 2!=0:
+...         print(i)
+... 
+...         
+1
+3
+5
+7
+9
+11
+13
+15
+>>> for i in range(1,11):
+...     print("5 x",i,"=",5*i)
+... 
+...     
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
