ec8d89044c9acce3bbfad383ab252c56d80b9d6d
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-02-23 21:35:46 +0530

    Add files via upload

---
+++ forloop_3.py
@@ -0,0 +1,33 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name="Atmiya"
+>>> for letter in name:
+...     print(letter)
+... 
+...     
+A
+t
+m
+i
+y
+a
+>>> total=0
+>>> for i in range (1,6):
+...     total=total+1
+...     print("sum is:",total)
+... 
+...     
+sum is: 1
+sum is: 2
+sum is: 3
+sum is: 4
+sum is: 5
+>>> numbers=[10,20,30,40]
+>>> for n in numbers:
+...     print(n)
+... 
+...     
+10
+20
+30
+40
